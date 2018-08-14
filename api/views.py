c6f133790555aeed592032b86daf73403582b508
Vivek Naik <39275096+vivekadishankara@users.noreply.github.com>
2018-08-14 21:56:33 +0530

    Facelift (#16)
    * flask_wtf and Bootstrap
    
    * icons and logos
    
    * added new search

--- api/views.py
+++ api/views.py
@@ -5,21 +5,23 @@ from flask import request, redirect, url_for
 from api import db
 from api.models import Item, Result
 
-import configuration as config
+from api.forms import SearchForm
 from metashopper.shopper import simple_search, ITEM_ATTR
 
 
 SHOPS = ['Amazon', 'Flipkart', 'Snapdeal']
+NEW = 1
 
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    form = SearchForm()
+    return render_template('home.html', form=form)
 
 
 @app.route('/results1', methods=['POST'])
 def results1():
-    item = request.form['q']
+    item = request.form['item']
     try:
         pums = request.form['gums']
         print(pums)
@@ -32,10 +34,15 @@ def results1():
         a = eval(line)
         res.append(a)
     results = {}
-    for i in range(len(SHOPS)):
-        results[SHOPS[i]] = res[(i)*5 : (i+1)*5-1]
+    form = SearchForm()
+    if form.submit_new.data:
+        for i in range(len(SHOPS)):
+            results[SHOPS[i]] = res[(i) * 3: (i + 1) * 3 - 1]
+    else:
+        for i in range(len(SHOPS)):
+            results[SHOPS[i]] = res[(i)*5 : (i+1)*5-1]
 
-    return render_template('results.html', item=item, shops=SHOPS, results=results)
+    return render_template('results.html', item=item, shops=SHOPS, results=results, form=form)
 
 
 @app.route('/results2', methods=['POST'])
@@ -50,10 +57,24 @@ def results2():
 
 @app.route('/results', methods=['POST'])
 def results():
-    item, num, nums = get_search_items()
+    form = SearchForm()
+    item, num, nums = form.get_search_items()
+
     item_filter = Item.query.filter_by(itemname=item).first()
 
+    global NEW
+    if form.submit_new.data and NEW:
+        try:
+            NEW = 0
+            item_filter.results.delete()
+            item_filter.query.delete()
+            db.session.commit()
+            item_filter = None
+        except AttributeError:
+            pass
+
     if item_filter:
+        NEW = 1
         search_results = {}
         for shop, num_i in zip(SHOPS, nums):
             results_filter = item_filter.results.filter_by(shop=shop).all()
@@ -68,12 +89,13 @@ def results():
             search_results[shop] = shop_results
     else:
         return redirect(url_for('results_new'), code=307)  # code=307 for post method
-    return render_template('results.html', item=item, shops=SHOPS, results=search_results)
+    return render_template('results.html', item=item, shops=SHOPS, results=search_results, form=form)
 
 
 @app.route('/results_new', methods=['POST'])
 def results_new():
-    item, num, nums = get_search_items()
+    form = SearchForm()
+    item, num, nums = form.get_search_items()
     search_results = simple_search(item, SHOPS, nums)
     it = Item.query.filter_by(itemname=item).first()
     if it:
@@ -91,16 +113,6 @@ def results_new():
 
     db.session.commit()
     return redirect(url_for('results'), code=307)
-
-
-def get_search_items():
-    item = request.form['q']
-    num = int(request.form['nums'])
-    num_op = num
-    if num < config.SEARCH_RESERVE:
-        num_op = config.SEARCH_RESERVE
-    nums = [num_op, num_op, num_op]
-    return item, num, nums
 
 
 def read_csv():
